3cb56b77963a96c0c4dd8a23fd74aed421603199
brominaga <dyt2yv@virginia.edu>
2021-08-12 14:49:20 -0700

    Added stage component that tracks cursor position

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,38 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
+      <Stage/>
+    </div>
+  );
+}
+type StageState = {
+  x: number;
+  y: number;
+}
+
+class Stage extends React.Component{
+  state: StageState = {x : 0, y: 0};
+
+  onMouseMove(e: React.MouseEvent){
+    this.setState({x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY});
+  }
+
+  render(){
+    return(
+    <div onMouseMove={this.onMouseMove.bind(this)}>
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="Placeholder-text">
           Dotmation Home!
         </p>
-      </header>
-    </div>
-  );
+      
+        <h3>X: {this.state.x}</h3>
+        <h3>Y: {this.state.y}</h3>
+        </header>
+    </div>);
+  }
 }
 
 export default App;
